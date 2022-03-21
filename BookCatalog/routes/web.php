b1bc3f0acf6a17b1e6094c59ffb992d54d6c65c2
joseph-njogu <josephnjogu487@gmail.com>
2022-03-22 02:19:01 +0300

    Adding routes to the app

--- BookCatalog/routes/web.php
+++ BookCatalog/routes/web.php
@@ -27,6 +27,23 @@ $router->put('authors/{id}', ['uses' => 'AuthorController@update']);
 $router->post('authors', ['uses' => 'AuthorController@create']);
 $router->delete('authors/{id}', ['uses' => 'AuthorController@delete']);
 
+//Find books
+$router->get('books',  ['uses' => 'BookController@showAllAuthors']);
+$router->get('books/{id}', ['uses' => 'BookController@showOneAuthor']);
+
+//CRUD Books
+$router->put('books/{id}', ['uses' => 'BookController@update']);
+$router->post('books', ['uses' => 'BookController@create']);
+$router->delete('books/{id}', ['uses' => 'BookController@delete']);
+
+//Find Comments
+$router->get('comments',  ['uses' => 'CommentController@showAllAuthors']);
+$router->get('comments/{id}', ['uses' => 'CommentController@showOneAuthor']);
+
+//CRUD Books
+$router->put('comments/{id}', ['uses' => 'CommentController@update']);
+$router->post('comments', ['uses' => 'CommentController@create']);
+$router->delete('comments/{id}', ['uses' => 'CommentController@delete']);
 
 
 });
